87e167c8d56ac88a50011e37565552b7bd6144a8
AtefAnggaMalikFadilah <atefanggamf@gmail.com>
2025-01-18 14:39:59 +0700

    add module cors body-parser and fs

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
-const PORT = 2025
+const PORT = 2601
+
+app.use(cors())
+app.use(bodyParser())
 
 app.get('/', (req, res) => {
     res.status(200).send('Welcome !')
