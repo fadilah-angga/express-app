35bc1557445dd8aa418b113ec24724ba390a5138
AtefAnggaMalikFadilah <atefanggamf@gmail.com>
2025-01-18 15:30:06 +0700

    add method post and delete

--- index.js
+++ index.js
@@ -12,8 +12,24 @@ app.get('/', (req, res) => {
     res.status(200).send('Welcome !')
 })
 
+let products = JSON.parse(fs.readFileSync('./data/products.json'))
+
 app.get('/products', (req, res) => {
-    let products = JSON.parse(fs.readFileSync('./data/products.json'))
+    res.status(200).send(products)
+})
+
+app.post('/products', (req, res) => {
+    products.push(req.body)
+
+    fs.writeFileSync('./data/products.json', JSON.stringify(products))
+    res.status(200).send(products)
+})
+
+app.delete('/products', (req, res) => {
+    let idx = products.findIndex(product => product.id === req.query.id)
+    products.splice(idx, 1)
+
+    fs.writeFileSync('./data/products.json', JSON.stringify(products))
     res.status(200).send(products)
 })
 
